a8241618b0dc9e6bcb6f270d1a08e3ca68c36d3c
Your Name <you@example.com>
2022-08-10 09:52:47 +0500

    Initial commit

---
+++ DeliveryOrderProcessor.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DeliveryOrderProcessor;
+
+public static class DeliveryOrderProcessor
+{
+    [FunctionName("DeliveryOrderProcessor")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [CosmosDB("orders", "order-delivery", Connection = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody, new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        });
+
+        await documentsOut.AddAsync(data);
+        
+        return new OkObjectResult($"Success");
+    }
+}
